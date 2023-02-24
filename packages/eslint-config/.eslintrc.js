cef203d854f2c4585bbaebeaf7cc3a6001d5d760
Aylon Carrijo <aylon.gabriel@outlook.com>
2023-02-24 18:52:40 -0300

    feat(eslint): update ts and react rules

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,68 +1,62 @@
-const overrides = {
-  ts: {
-    files: ["*.ts", "*.tsx"],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: "./tsconfig.json",
-    },
-    settings: {
-      "import/parsers": {
-        "@typescript-eslint/parser": [".ts", ".tsx"],
-      },
-      "import/resolver": {
-        typescript: true,
-      },
-    },
-    plugins: ["@typescript-eslint"],
-    extends: [
-      "standard-with-typescript",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    ],
-    rules: {
-      "@typescript-eslint/explicit-member-accessibility": "error",
-      "@typescript-eslint/array-type": ["error", { default: "generic" }],
-      "@typescript-eslint/no-empty-function": [
-        "error",
-        { allow: ["private-constructors", "protected-constructors"] },
-      ],
-    },
-  },
-  react: {
-    files: ["*.jsx", "*.tsx"],
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    plugins: ["jsx-a11y", "react", "react-hooks"],
-    extends: [
-      "plugin:jsx-a11y/recommended",
-      "plugin:react/recommended",
-      "plugin:react/jsx-runtime",
-      "plugin:react-hooks/recommended",
-    ],
-    rules: {
-      "jsx-a11y/role-has-required-aria-props": "warn",
-      "jsx-a11y/role-supports-aria-props": "warn",
-      "jsx-a11y/aria-props": "warn",
-      "jsx-a11y/aria-proptypes": "warn",
-      "jsx-a11y/aria-unsupported-elements": "warn",
-      "jsx-a11y/alt-text": "warn",
-      "react/no-unknown-property": "off",
-      "react/prop-types": "off",
-      "react/jsx-no-target-blank": "off",
-      "react/button-has-type": "error",
-      "react/self-closing-comp": "error",
-      "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
-    },
-  },
-};
-
 module.exports = {
   overrides: [
-    overrides.ts,
-    overrides.react,
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      settings: {
+        "import/parsers": {
+          "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+        "import/resolver": {
+          typescript: true,
+        },
+      },
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "standard-with-typescript",
+      ],
+      rules: {
+        "@typescript-eslint/triple-slash-reference": "off",
+        "@typescript-eslint/explicit-member-accessibility": "error",
+        "@typescript-eslint/array-type": ["error", { default: "generic" }],
+        "@typescript-eslint/no-empty-function": [
+          "error",
+          { allow: ["private-constructors", "protected-constructors"] },
+        ],
+      },
+    },
+    {
+      files: ["*.jsx", "*.tsx"],
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+      plugins: ["jsx-a11y", "react", "react-hooks"],
+      extends: [
+        "plugin:jsx-a11y/recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+      ],
+      rules: {
+        "jsx-a11y/alt-text": [
+          "error",
+          { elements: ["img"], img: ["Image", "Img"] },
+        ],
+        "react/jsx-no-target-blank": "off",
+        "react/no-unknown-property": "off",
+        "react/prop-types": "off",
+        "react/button-has-type": "error",
+        "react/self-closing-comp": "error",
+        "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
+      },
+    },
     { files: "*", extends: ["prettier"] },
   ],
   env: {
